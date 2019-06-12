93591e086628f701ebbbfb3b7c8010918457445f
Leo Li <327206596@qq.com>
2019-06-12 22:31:11 +1000

    trouble shooting for 404

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
+// import {BrowserRouter} from 'react-router-dom';
 import Routes from './routes';
 import TopNav from './components/app/topnav';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
+
+      <div className="App">TopNav
         <TopNav></TopNav>
         {/* <div className="container"> */}
             <Routes></Routes>
         {/* </div> */}
         
       </div>
-    </BrowserRouter>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './style/index.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App>Hello world</App>
+    </BrowserRouter>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
